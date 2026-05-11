e0284d9b7c0e0d7dc06c1045bdaf31e8cac34733
Fatma Zehra Şener <fatmazehraenerfatmazehra.42@gmail.com>
2026-05-11 20:22:41 +0300

    Feat:Completed model relations.

--- src/model/Transaction.java
+++ src/model/Transaction.java
@@ -5,13 +5,34 @@ public class Transaction {
 	private String id;
     private String type;
     private double amount;
+    
+    private Company company; //bu islem hangi sirkete ait?
+    private Invoice invoice; //bu isleme bagli bir fatura var mi?
 
     public Transaction() {}
 
-    public Transaction(String id, String type, double amount) {
+    public Transaction(String id, String type, double amount,Company company) {
         this.id = id;
         this.type = type;
         this.amount = amount;
+        this.company=company;
+    }
+    
+    public Company getCompany() {
+    	return company;
+    }
+    
+    public void setCompany(Company company) {
+    	this.company=company;
+    }
+    
+    public Invoice getInvoice() {
+    	return invoice;
+    }
+    
+    public void setInvoice(Invoice invoice)
+    {
+    	this.invoice=invoice;
     }
 
     public String getId() {
