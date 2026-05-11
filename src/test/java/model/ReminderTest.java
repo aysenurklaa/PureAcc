79e4b7bbab0f972fd4e86fbb3d54b10856d32fda
Safiyeural <safiyeural4@gmail.com>
2026-05-12 00:27:08 +0300

    Create ReminderTest.java

---
+++ src/test/java/model/ReminderTest.java
@@ -0,0 +1,20 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Date;
+
+// Hatırlatma modelinin temel fonksiyonlarını test eden sınıf.
+public class ReminderTest {
+
+    @Test
+    public void testReminderCreation() {
+        // Yeni bir hatırlatma planı oluşturalım
+        Date scheduleDate = new Date();
+        Reminder reminder = new Reminder("SMS", scheduleDate);
+
+        // Bilgileri doğrulayalım
+        assertEquals("SMS", reminder.getChannel()); // Kanal doğru mu?
+        assertEquals(scheduleDate, reminder.getScheduledAt()); // Planlanan tarih doğru mu?
+    }
+}
